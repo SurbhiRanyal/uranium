48ea65e3beba840837703a9bde27f7156773829a
Surbhi Ranyal <surbhisahotra231@gmail.com>
2022-04-07 19:42:51 +0530

    surbhi

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+let welcome = function() {
+
+    console.log("Welcome to my application. I am Surbhi and a part of FunctionUp Uranium cohort.")
+}
+
+module.exports.welcomeMessage = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,15 @@
+let trim = function(){
+    console.log('The result after trimming the string is :' + ' functionUp batch  '.trim() + '.')
+   }
+   
+   let changetoLowerCase = function() {
+       console.log('The result after using toLowerCase function is :', 'SURBHI '.toLowerCase())
+   }
+   
+   let changeToUpperCase = function() {
+       console.log('The result after using toUpperCase function is ', 'functionup'.toUpperCase())    
+   }
+   
+module.exports.trimString = trim
+module.exports.changeCaseToLower = changetoLowerCase
+module.exports.changeCaseToUpper = changeToUpperCase
